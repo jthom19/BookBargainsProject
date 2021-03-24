9508401bb040f190d84265c3f880d79c1d8a06b3
Maria Ringes <m.ringes@me.com>
2021-03-24 19:49:11 -0400

    Add login, register, messaging

---
+++ bookbargains/bbapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from bbapp import views
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('messaging', views.messaging, name='messaging'),
+]
--- bookbargains/bbapp/views.py
+++ bookbargains/bbapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("I am the first page")
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'signup.html')
+
+def messaging(request):
+    return render(request, 'messaging.html')
