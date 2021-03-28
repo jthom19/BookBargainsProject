766c607ecda5ffebc357c5f5a93c258469870cb2
Maria Ringes <m.ringes@me.com>
2021-03-28 18:59:01 -0400

    connecting home page

--- bookbargains/bbapp/urls.py
+++ bookbargains/bbapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('login', views.userLog, name='login'),
     path('register', views.register, name='register'),
     path('messaging', views.messaging, name='messaging'),
-    path('', views.home_view, name="home"),
+    path('', views.home, name="home"),
     path('signup/', views.signup_view, name="signup"),
 ]
