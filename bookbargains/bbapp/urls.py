d8e1e9e3448dd9b7c83d9a80da707df031edb0fd
mringes13 <60116121+mringes13@users.noreply.github.com>
2021-03-28 18:46:25 -0400

    Update urls.py

--- bookbargains/bbapp/urls.py
+++ bookbargains/bbapp/urls.py
@@ -5,11 +5,6 @@ urlpatterns = [
     path('login', views.userLog, name='login'),
     path('register', views.register, name='register'),
     path('messaging', views.messaging, name='messaging'),
-<<<<<<< HEAD
     path('', views.home_view, name="home"),
     path('signup/', views.signup_view, name="signup"),
-=======
-    path('', views.home, name="home"),
-    path('signup/', views.signup_view, name="signup")
->>>>>>> 08d6f3ffeda71323a860968311362a702208d71f
 ]
