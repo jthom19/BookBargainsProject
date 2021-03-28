622e96b511d86f1307764a10871392f2be038d54
Shahzad Manji <shahzadmanji1@gmail.com>
2021-03-28 18:05:46 -0400

    forms and views

--- bookbargains/bbapp/urls.py
+++ bookbargains/bbapp/urls.py
@@ -4,5 +4,7 @@ from bbapp import views
 urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
-    path('messaging', views.messaging, name='messaging'),
+    path('messaging', views.messaging, name='messaging')
+    path('', home_view, name="home")
+    path('signup/', signup_view, name="signup")
 ]
--- bookbargains/bbapp/views.py
+++ bookbargains/bbapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 # Create your views here.
+
 
 def login(request):
     return render(request, 'login.html')
@@ -10,3 +15,19 @@ def register(request):
 
 def messaging(request):
     return render(request, 'messaging.html')
+
+def home_view(request):
+    return render(request, 'LandingPage.html')
+
+def signup_view(request):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('LandingPage')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
