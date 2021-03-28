0411034ad8a71272bacb5f653c4f0e93bbc75299
mringes13 <60116121+mringes13@users.noreply.github.com>
2021-03-28 16:38:03 -0400

    Delete bookbargains directory

--- bookbargains/bbapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BbappConfig(AppConfig):
-    name = 'bbapp'
--- bookbargains/bbapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from bbapp import views
-
-urlpatterns = [
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('messaging', views.messaging, name='messaging'),
-]
