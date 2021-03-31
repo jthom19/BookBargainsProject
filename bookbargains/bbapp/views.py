742352087d539890bd4691ea77d3baa1d034cb0d
Maria Ringes <m.ringes@me.com>
2021-03-30 21:54:40 -0400

    user profile creation

--- bookbargains/bbapp/views.py
+++ bookbargains/bbapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from .forms import CreateUserForm, CreateProfileForm
 # Create your views here.
 
 
@@ -26,12 +26,27 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.email = form.cleaned_data.get('email')
             user.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
-            return redirect('/')
+            return redirect('../profile/')
     else:
         form = CreateUserForm()
     return render(request, 'register.html', {'form': form})
+
+def createprofile(request):
+    p_form = CreateProfileForm()
+    if request.method == "POST":
+        form = CreateProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            user.Profile.first_name = form.cleaned_data.get('first_name')
+            user.Profile.last_name = form.cleaned_data.get('last_name')
+            user.Profile.phone = form.cleaned_data.get('phone')
+            user.Profile.major = form.cleaned_data.get('major')
+            user.Profile.housing = form.cleaned_data.get('housing')
+            user.Profile.save()
+        else:
+            form = CreateProfileForm()
+    return render(request, 'createprofile.html', {'p_form':p_form})
