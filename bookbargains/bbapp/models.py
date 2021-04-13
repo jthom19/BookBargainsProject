797fff4f377238aa6c8e701f2dd574fa16f18f03
Joseph Porpora <31698856+JosephPorpora@users.noreply.github.com>
2021-04-13 12:30:38 -0400

    Update models.py
    Added Book model and choices for dropdowns

--- bookbargains/bbapp/models.py
+++ bookbargains/bbapp/models.py
@@ -17,3 +17,55 @@ class Profile(models.Model):
         return self.user.username
 
 
+BOOK_CONDITION_CHOICES = (('SO', 'Select One'), ('NE', 'New'), ('GR', 'Great'),
+                          ('GO', 'Good'), ('AV', 'Average'), ('PO', 'Poor'))
+
+FIELD_CHOICES = (
+    ('SO', 'Select One'),
+    ('ART', 'Arts'),
+    ('MSC', 'Music'),
+    ('HST', 'History'),
+    ('LNG', 'Languages'),
+    ('LAW', 'Law'),
+    ('PHL', 'Philosophy'),
+    ('THE', 'Theology'),
+    ('ECN', 'Economics'),
+    ('PLS', 'Political Science'),
+    ('PSY', 'Psychology'),
+    ('SOC', 'Sociology'),
+    ('NUR', 'Nursing'),
+    ('BIO', 'Biology'),
+    ('CHM', 'Chemistry'),
+    ('PHY', 'Physics'),
+    ('ENG', 'Engineering'),
+    ('CSC', 'Computer Science'),
+    ('MTH', 'Mathematics'),
+    ('BUS', 'Business'),
+    ('FIN', 'Finance'),
+    ('ACT', 'Accounting'), )
+
+SELL_DONATE_CHOICES = (('SO', 'Select One'), ('SE', 'Selling'), ('DO',
+                                                                 'Donating'))
+
+class Book(models.Model):
+    #need primary key
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=False)
+    selldonate = models.CharField(
+        max_length=2,
+        choices=SELL_DONATE_CHOICES,
+        default='Select One',
+        null=True)
+    title = models.CharField(max_length=100, null=True)
+    author = models.CharField(max_length=100, null=True)
+    isbn = models.CharField(max_length=13, null=True)
+    edition = models.CharField(max_length=100, null=True)
+    condition = models.CharField(
+        max_length=2,
+        choices=BOOK_CONDITION_CHOICES,
+        default='Select One',
+        null=True)  #dropdown
+    field = models.CharField(
+        max_length=4, choices=FIELD_CHOICES, default='Select One',
+        null=True)  #dropdown
+    price = models.CharField(max_length=100, null=True)
