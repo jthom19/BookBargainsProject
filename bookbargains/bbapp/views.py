c92726a528ba8108ee912f1de46a0a9ae06a1c03
Joseph Porpora <31698856+JosephPorpora@users.noreply.github.com>
2021-04-25 23:47:46 -0400

    Added better filtering to search

--- bookbargains/bbapp/views.py
+++ bookbargains/bbapp/views.py
@@ -36,13 +36,13 @@ class HomePageView(ListView):
 class SearchResultsView(ListView):
     model = Book
     template_name = 'searchresults.html'
-    def get_queryset(self): # new
+    def get_queryset(self):  # new
         query = self.request.GET.get('q')
         object_list = Book.objects.filter(
-            Q(title__icontains=query) | Q(author__icontains=query)
-        )
+            Q(title__icontains=query) | Q(author__icontains=query) | Q(
+                ISBN13__icontains=query) | Q(edition__icontains=query) | Q(
+                    condition__icontains=query) | Q(field__icontains=query))
         return object_list
-
 
 
 def signup(request):
